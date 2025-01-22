afffab0cbd9d4a0a1b55d2ff91379d7bf99704a0
Otavio <otavio25emilio@gmail.com>
2025-01-22 14:18:22 -0300

    finished the first part

--- jogo.js
+++ jogo.js
@@ -3,92 +3,140 @@ sprites.src = './sprites.png';
 
 
 const canvas = document.querySelector('#game-canvas');
-const contexto = canvas.getContext('2d');
-
-
-contexto.fillStyle = '#70c5ce';
-contexto.fillRect(0,0, canvas.width, canvas.heigth)
-
-
-
+const contexto = canvas.getContext('2d')
+contexto.fillStyle='#70c5ce';
+const flappyBird = {
+    spriteX:0,
+    spriteY:0,
+    largura:35,
+    altura:25,
+    x:10,
+    y:50,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY,
+            flappyBird.largura, flappyBird.altura,
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
+    },
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza(){
+        flappyBird.velocidade += flappyBird.gravidade;
+        flappyBird.y = flappyBird.y + flappyBird.velocidade;
+        flappyBird.x = flappyBird.x + 2;
+    }
+}
 const cenario = {
-    spriteX: 390,
-    spriteY: 0,
-    largura: 275,
-    altura: 204,
-    x: 0,
-    y: 164,
-        desenha() {
+    spriteX:390,
+    spriteY:0,
+    largura:275,
+    altura:204,
+    x:0,
+    y:280,
+    desenha(){
+        contexto.fillRect(0,0, canvas.width, canvas.height)
         contexto.drawImage(
             sprites,
             cenario.spriteX, cenario.spriteY,
             cenario.largura, cenario.altura,
             cenario.x, cenario.y,
-            cenario.largura, cenario.altura
+            cenario.largura, cenario.altura,
+
+
+
+
         );
         contexto.drawImage(
             sprites,
             cenario.spriteX, cenario.spriteY,
             cenario.largura, cenario.altura,
             cenario.x + cenario.largura, cenario.y,
-            cenario.largura, cenario.altura
+            cenario.largura, cenario.altura,
+           
         );
     }
 }
-
-
 const chao = {
-    spriteX: 0,
-    spriteY: 609,
-    largura: 223,
-    altura: 113,
-    x: 0,
-    y: 367,
-        desenha() {
+    spriteX:0,
+    spriteY:610,
+    largura:223,
+    altura:111,
+    x:0,
+    y:370,
+    desenha(){
         contexto.drawImage(
             sprites,
             chao.spriteX, chao.spriteY,
             chao.largura, chao.altura,
             chao.x, chao.y,
-            chao.largura, chao.altura
+            chao.largura, chao.altura,
         );
         contexto.drawImage(
             sprites,
             chao.spriteX, chao.spriteY,
             chao.largura, chao.altura,
             chao.x + chao.largura, chao.y,
-            chao.largura, chao.altura
+            chao.largura, chao.altura,
+           
         );
     }
 }
-
-
-const flappyBird = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 35,
-    altura: 25,
-    x: 10,
-    y: 50,
-        desenha() {
+const inicio = {
+    spriteX:130,
+    spriteY:0,
+    largura:180,
+    altura:152,
+    x:70,
+    y:70,
+    desenha(){
         contexto.drawImage(
             sprites,
-            flappyBird.spriteX, flappyBird.spriteY,
-            flappyBird.largura, flappyBird.altura,
-            flappyBird.x, flappyBird.y,
-            flappyBird.largura, flappyBird.altura
+            inicio.spriteX, inicio.spriteY,
+            inicio.largura, inicio.altura,
+            inicio.x, inicio.y,
+            inicio.largura, inicio.altura,
         );
     }
 }
-
-
-function loop() {
-    flappyBird.desenha();
-    chao.desenha();
-    cenario.desenha();
-
-
-    requestAnimationFrame(loop);
+const TelaIncio = {
+    desenha(){
+        cenario.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        inicio.desenha();
+    },
+    click(){
+        telaAtiva = TelaJogo;
+    }
 }
-loop();
+var telaAtiva = TelaIncio;
+
+
+function mudaTelaAtiva(){
+    telaAtiva.click();
+}
+const TelaJogo = {
+    desenha(){
+        cenario.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        flappyBird.atualiza();
+    },
+    click(){}
+}
+window.addEventListener("click", mudaTelaAtiva)
+function loop(){
+    telaAtiva.desenha()
+    requestAnimationFrame(loop)
+}
+
+
+
+
+loop()
+
+
 
